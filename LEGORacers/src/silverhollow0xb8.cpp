80207c535d67d849d79f4325ab11e47495501f00
foxtacles <mail@csemmler.com>
2026-04-20 12:39:40 -0700

    Implement 2 STUBs in `LegoRacers` / class skeletons (#79)
    * Implement 2 STUBs in `LegoRacers` / class skeletons
    
    * Fix vtable

---
+++ LEGORacers/src/silverhollow0xb8.cpp
@@ -0,0 +1,17 @@
+#include "silverhollow0xb8.h"
+
+DECOMP_SIZE_ASSERT(SilverHollow0xb8, 0xb8)
+
+// STUB: LEGORACERS 0x00411150
+SilverHollow0xb8::SilverHollow0xb8()
+{
+	// TODO
+	STUB(0x411150);
+}
+
+// STUB: LEGORACERS 0x004113c0
+void SilverHollow0xb8::VTable0x00()
+{
+	// TODO
+	STUB(0x4113c0);
+}
