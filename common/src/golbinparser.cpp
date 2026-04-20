93a98a928e4bc801b86deb5e8ecddde12cb5a0be
MS <disinvite@users.noreply.github.com>
2026-04-20 10:32:56 -0400

    Implement some `Gol*Parser` functions (#78)
    * Implement some Gol*Parser functions
    
    * GOLDP fixes
    
    * More functions
    
    * Fix typo

--- common/src/golbinparser.cpp
+++ common/src/golbinparser.cpp
@@ -1,5 +1,13 @@
 #include "golbinparser.h"
 
+#include "golerror.h"
+
+#include <stdio.h>
+
+// GLOBAL: GOLDP 0x1005f030
+// GLOBAL: LEGORACERS 0x004c182c
+const LegoChar* g_fileFormatStr = "file %s\n";
+
 // STUB: LEGORACERS 0x0044a7e0
 GolBinParser::GolBinParser()
 {
@@ -52,12 +60,24 @@ undefined4 GolBinParser::VTable0x48(undefined4, undefined4)
 	return 1;
 }
 
-// STUB: GOLDP 0x10030e40
-// STUB: LEGORACERS 0x0044b1e0
-void GolBinParser::VTable0x40(undefined4)
+// FUNCTION: GOLDP 0x10030e40
+// FUNCTION: LEGORACERS 0x0044b1e0
+void GolBinParser::VTable0x40(undefined4 p_code)
 {
-	// TODO
-	STUB(0x0044b1e0);
+	if (m_filePath == NULL) {
+		GOL_FATALERROR_MESSAGE(ParserErrorCodeToString(p_code));
+		return;
+	}
+
+	LegoS32 totalLen = strlen(ParserErrorCodeToString(p_code)) + strlen(m_filePath) + strlen(g_fileFormatStr);
+	m_unk0xa4[0] = '\0';
+
+	if (totalLen < (LegoS32) sizeOfArray(m_unk0xa4) - 1) {
+		sprintf(m_unk0xa4, g_fileFormatStr, m_filePath);
+	}
+
+	strcat(m_unk0xa4, ParserErrorCodeToString(p_code));
+	GOL_FATALERROR_MESSAGE(m_unk0xa4);
 }
 
 // STUB: GOLDP 0x10030f10
@@ -70,26 +90,50 @@ void GolBinParser::VTable0x4c()
 
 // STUB: GOLDP 0x10031070
 // STUB: LEGORACERS 0x0044b410
-void GolBinParser::VTable0x50()
+void GolBinParser::VTable0x50(undefined4)
 {
 	// TODO
 	STUB(0x0044b410);
 }
 
-// STUB: GOLDP 0x10031100
-// STUB: LEGORACERS 0x0044b4a0
-void GolBinParser::VTable0x54()
+// FUNCTION: GOLDP 0x10031100
+// FUNCTION: LEGORACERS 0x0044b4a0
+void GolBinParser::VTable0x54(undefined4 p_param)
 {
-	// TODO
-	STUB(0x0044b4a0);
+	*m_unk0xa4 = p_param;
+	LegoS32 code = BufferedWrite(m_unk0x1f0, m_unk0xa4, 1);
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0++;
 }
 
-// STUB: GOLDP 0x10031140
-// STUB: LEGORACERS 0x0044b4e0
-void GolBinParser::VTable0x58()
+// FUNCTION: GOLDP 0x10031140
+// FUNCTION: LEGORACERS 0x0044b4e0
+void GolBinParser::WriteFloat(LegoFloat p_param)
 {
-	// TODO
-	STUB(0x0044b4e0);
+	*m_unk0xa4 = 3;
+	LegoS32 code = BufferedWrite(m_unk0x1f0, m_unk0xa4, 1);
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0++;
+
+	LegoU32 buf;
+	memcpy(&buf, &p_param, sizeof(buf));
+	m_unk0xa4[0] = (LegoU8) buf;
+	m_unk0xa4[1] = (LegoU8) (buf >> 8);
+	m_unk0xa4[2] = (LegoU8) (buf >> 16);
+	m_unk0xa4[3] = (LegoU8) (buf >> 24);
+
+	code = BufferedWrite(m_unk0x1f0, m_unk0xa4, sizeof(p_param));
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0 += 4;
 }
 
 // STUB: GOLDP 0x1002fd50 FOLDED
@@ -100,37 +144,85 @@ void GolBinParser::VTable0x5c()
 	STUB(0x0044b570);
 }
 
-// STUB: GOLDP 0x100311d0
-// STUB: LEGORACERS 0x0044b580
-void GolBinParser::VTable0x60()
+// FUNCTION: GOLDP 0x100311d0
+// FUNCTION: LEGORACERS 0x0044b580
+void GolBinParser::WriteInt4(undefined4 p_param)
 {
-	// TODO
-	STUB(0x0044b580);
+	*m_unk0xa4 = 4;
+	LegoS32 code = BufferedWrite(m_unk0x1f0, m_unk0xa4, 1);
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0++;
+
+	m_unk0xa4[0] = (LegoU8) p_param;
+	m_unk0xa4[1] = (LegoU8) (p_param >> 8);
+	m_unk0xa4[2] = (LegoU8) (p_param >> 16);
+	m_unk0xa4[3] = (LegoU8) (p_param >> 24);
+
+	code = BufferedWrite(m_unk0x1f0, m_unk0xa4, sizeof(p_param));
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0 += 4;
 }
 
-// STUB: GOLDP 0x10031260
-// STUB: LEGORACERS 0x0044b610
-void GolBinParser::VTable0x64()
+// FUNCTION: GOLDP 0x10031260
+// FUNCTION: LEGORACERS 0x0044b610
+void GolBinParser::WriteString(LegoChar* p_str)
 {
-	// TODO
-	STUB(0x0044b610);
+	*m_unk0xa4 = 2;
+	LegoS32 code = BufferedWrite(m_unk0x1f0, m_unk0xa4, 1);
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0++;
+
+	LegoS32 len = strlen(p_str);
+	if (len >= 63) {
+		len = 63;
+	}
+
+	memcpy(m_unk0xa4, p_str, len);
+
+	code = BufferedWrite(m_unk0x1f0, m_unk0xa4, len);
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0 += len;
+
+	*m_unk0xa4 = 2;
+	code = BufferedWrite(m_unk0x1f0, m_unk0xa4, 1);
+	if (code != e_ioSuccess) {
+		FUN_10032580(code);
+	}
+
+	m_unk0x1f0++;
 }
 
+// FUNCTION: GOLDP 0x10029920 FOLDED
 // FUNCTION: LEGORACERS 0x004164c0 FOLDED
 void GolBinParser::VTable0x68()
 {
 }
 
+// FUNCTION: GOLDP 0x10029920 FOLDED
 // FUNCTION: LEGORACERS 0x004164c0 FOLDED
 void GolBinParser::VTable0x6c()
 {
 }
 
+// FUNCTION: GOLDP 0x10029920 FOLDED
 // FUNCTION: LEGORACERS 0x004164c0 FOLDED
 void GolBinParser::VTable0x70()
 {
 }
 
+// FUNCTION: GOLDP 0x1002c020 FOLDED
 // FUNCTION: LEGORACERS 0x004513d0 FOLDED
 void GolBinParser::VTable0x74(undefined4)
 {
